37200850718c7c52156c3835c25e5d87e7ff2b44
NullpointerException <x_tacy@163.com>
2016-11-08 01:58:40 +0000

    initial commit

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #
 #     DATABASEURI = "postgresql://gravano:foobar@w4111a.eastus.cloudapp.azure.com/proj1part2"
 #
-DATABASEURI = "postgresql://user:password@w4111vm.eastus.cloudapp.azure.com/w4111"
+DATABASEURI = "postgresql://zw2364:6005@w4111vm.eastus.cloudapp.azure.com/w4111"
 
 
 #
